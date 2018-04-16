6fc5e1860c06edeb776d4047cb93361728d93537
Jim Eng <jimeng@umich.edu>
2018-04-16 16:04:33 -0400

    Add validations for submitting a session request

--- app/models/session_request.rb
+++ app/models/session_request.rb
@@ -1,10 +1,35 @@
 # Requesting and scheduling an Instructional Session is a multi-step process.
-# The process starts when someone creates a request for a session with an
-# initial state of "draft".  More editing may occur, but the next step is
-# to submit it for scheduling, which changes its state to "requested".
-# More changes may be made, including the following:
+# This process is encapsulated in a SessionRequest object.
 #
-#   - Reviewing the request and filling in details
+# The process starts when someone creates a SessionRequest object with an
+# initial state of "draft".  The significant attributes of a SessionRequest
+# that might be provided include:
+#
+#   - Whether the session is course-related.
+#     If course related:
+#       * Identity of the faculty
+#       * Term in which the session will be taught
+#       * Class section(s) attending the session
+#       * Requested session times based on when the section meets
+#     Otherwise
+#       * Identity of the contact-person for the session
+#       * Title of the session
+#       * Requested session times
+#       * Expected attendance
+#   - Topics for the session
+#   - Needed accommodations
+#   - Whether a library location is needed
+#   - Whether an evaluation is needed (and which one?)
+#   - Whether registration is needed
+#   - Possibly attachments with relevant information (e.g. a syllabus
+#     for a course, or an assignment the participants will be doing)
+#   - Further comments from the requester
+#
+# More editing may occur, but the next step is
+# to submit the SessionRequest for scheduling, which changes its state to
+# "requested".  More changes may be made, including the following:
+#
+#   - Reviewing the request and updating attributes
 #   - Setting start-time and end-time
 #   - Adding instructor(s)
 #   - Adding a room (or marking not needed)
@@ -16,11 +41,72 @@
 # Session is created with all relevant information from the Session
 # Request.  Later, details about the session might change, but there's
 # no need to update the Session Request once it is scheduled.
-
+#
 class SessionRequest
-  attr_accessor :requested_by
+  attr_accessor :requested_by, :title, :contact_person, :accommodations, :expected_attendance,
+                :library_location_needed, :evaluation_needed, :registration_needed, :requested_times, :topics,
+                :course_related
+  attr_reader :state
 
   def initialize(attribute_hash = {})
-    self.requested_by = attribute_hash[:requested_by]
+    @state = 'draft'
+    self.requested_by             = attribute_hash[:requested_by]
+    self.contact_person           = attribute_hash[:contact_person]
+    self.title                    = attribute_hash[:title]
+    self.course_related           = attribute_hash[:course_related]
+    self.expected_attendance      = attribute_hash[:expected_attendance]
+    self.accommodations           = attribute_hash[:accommodations]
+    self.library_location_needed  = attribute_hash[:library_location_needed]
+    self.evaluation_needed        = attribute_hash[:evaluation_needed]
+    self.registration_needed      = attribute_hash[:registration_needed]
+    if attribute_hash.has_key?(:requested_times) && ! attribute_hash[:requested_times].nil?
+      self.requested_times = []
+      attribute_hash[:requested_times].each {|value|
+        if value.date && value.start_time && value.end_time
+          self.requested_times << TimeRange.new(
+              value.date.strftime("%Y-%m-%d"),
+              value.start_time.strftime("%I:%M%p"),
+              value.end_time.strftime("%I:%M%p")
+          )
+        end
+      }
+    end
+    if attribute_hash.has_key?(:topics) && ! attribute_hash[:topics].nil?
+      self.topics = []
+      attribute_hash[:topics].each {|value|
+        self.topics << value
+      }
+    end
+  end
+
+  def submitted
+    result = ModelValidations.validate_to_submit_for_scheduling(to_h)
+    if result.failure?
+      # report errors
+    else
+      @state = 'requested'
+    end
+
+  end
+
+  def draft?
+    @state == 'draft'
+  end
+
+  def to_h
+    {
+      requested_by: requested_by,
+      contact_person: contact_person,
+      course_related: course_related,
+      title: title,
+      expected_attendance: expected_attendance,
+      accommodations: accommodations,
+      library_location_needed: library_location_needed,
+      evaluation_needed: evaluation_needed,
+      registration_needed: registration_needed,
+      requested_times: requested_times,
+      topics: topics
+    }
+
   end
 end
--- spec/models/academic_term_spec.rb
+++ spec/models/academic_term_spec.rb
@@ -23,11 +23,11 @@ describe AcademicTerm do
 
     it "returns FA if parameter starting with FA or FALL (any case) is provided to constructor" do
       [
-          ' fa 2018 ', 'Fa   2018 ', 'fA 2018 ', "FA2018",
-          ' fall 2018 ', ' falL 2018 ', ' faLl 2018 ', ' faLL 2018 ',
-          ' fAll 2018 ', ' fAlL 2018 ', ' fALl 2018 ', ' fALL 2018 ',
-          ' Fall 2018 ', ' FalL 2018 ', ' FaLl 2018 ', ' FaLL 2018 ',
-          ' FAll 2018 ', ' FAlL 2018 ', ' FALl 2018 ', ' FALL 2018 '
+        ' fa 2018 ', 'Fa   2018 ', 'fA 2018 ', "FA2018",
+        ' fall 2018 ', ' falL 2018 ', ' faLl 2018 ', ' faLL 2018 ',
+        ' fAll 2018 ', ' fAlL 2018 ', ' fALl 2018 ', ' fALL 2018 ',
+        ' Fall 2018 ', ' FalL 2018 ', ' FaLl 2018 ', ' FaLL 2018 ',
+        ' FAll 2018 ', ' FAlL 2018 ', ' FALl 2018 ', ' FALL 2018 '
       ].each do |param|
         expect(AcademicTerm.new(param).semester).to eq "FA"
       end
@@ -70,11 +70,11 @@ describe AcademicTerm do
 
     it "returns SU if parameter starting with SU or SUMMER (any case) is provided to constructor" do
       [
-          ' su 2018 ', 'Su   2018 ', 'sU 2018 ', "SU2018",
-          ' summer 2018 ', ' Summer 2018 ', ' SummeR 2018 ', ' SummER 2018 ',
-          ' suMMer 2018 ', ' SUmmER 2018 ', ' SUMmer 2018 ', ' SuMmEr 2018 ',
-          ' SuMMeR 2018 ', ' sUMMER 2018 ', ' sUMMEr 2018 ', ' sUMMer 2018 ',
-          ' suMmeR 2018 ', ' sUmmer 2018 ', ' SUmmer 2018 ', ' SUMMER 2018 '
+        ' su 2018 ', 'Su   2018 ', 'sU 2018 ', "SU2018",
+        ' summer 2018 ', ' Summer 2018 ', ' SummeR 2018 ', ' SummER 2018 ',
+        ' suMMer 2018 ', ' SUmmER 2018 ', ' SUMmer 2018 ', ' SuMmEr 2018 ',
+        ' SuMMeR 2018 ', ' sUMMER 2018 ', ' sUMMEr 2018 ', ' sUMMer 2018 ',
+        ' suMmeR 2018 ', ' sUmmer 2018 ', ' SUmmer 2018 ', ' SUMMER 2018 '
       ].each do |param|
         expect(AcademicTerm.new(param).semester).to eq "SU"
       end
@@ -82,9 +82,9 @@ describe AcademicTerm do
 
     it "returns FA if parameter starting with FA or FALL (any case) is provided to constructor" do
       [
-          ' wi 2018 ', 'wI   2018 ', 'Wi 2018 ', "WI 2018",
-          ' winter 2018 ', ' winteR 2018 ', ' wintER 2018 ', ' winTER 2018 ',
-          ' wiNter 2018 ', ' wInTER 2018 ', ' WINter 2018 ', ' WINTER 2018 '
+        ' wi 2018 ', 'wI   2018 ', 'Wi 2018 ', "WI 2018",
+        ' winter 2018 ', ' winteR 2018 ', ' wintER 2018 ', ' winTER 2018 ',
+        ' wiNter 2018 ', ' wInTER 2018 ', ' WINter 2018 ', ' WINTER 2018 '
       ].each do |param|
         expect(AcademicTerm.new(param).semester).to eq "WI"
       end
